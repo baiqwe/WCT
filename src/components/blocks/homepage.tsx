dd368329138b233ea8525478b5a2ad6b27a1b9ac
javayhu <javayhu@gmail.com>
2026-03-07 11:42:11 +0800

    chore: reset homepage layout

--- src/components/blocks/homepage.tsx
+++ src/components/blocks/homepage.tsx
@@ -1,45 +1,31 @@
 import HeroSection from '@/components/blocks/hero';
 import LogoCloudSection from '@/components/blocks/logo-cloud';
-import { lazy, Suspense } from 'react';
-
-const FeaturesSection = lazy(() => import('@/components/blocks/features'));
-const Features2Section = lazy(() => import('@/components/blocks/features2'));
-const CallToActionSection = lazy(
-  () => import('@/components/blocks/calltoaction')
-);
-const StatsSection = lazy(() => import('@/components/blocks/stats'));
-const IntegrationSection = lazy(
-  () => import('@/components/blocks/integration')
-);
-const Integration2Section = lazy(
-  () => import('@/components/blocks/integration2')
-);
-const PricingSection = lazy(() => import('@/components/blocks/pricing'));
-const FaqSection = lazy(() => import('@/components/blocks/faqs'));
-const TestimonialsSection = lazy(
-  () => import('@/components/blocks/testimonials')
-);
-const NewsletterCard = lazy(
-  () => import('@/components/blocks/newsletter-card')
-);
+import FeaturesSection from '@/components/blocks/features';
+import Features2Section from '@/components/blocks/features2';
+import CallToActionSection from '@/components/blocks/calltoaction';
+import StatsSection from '@/components/blocks/stats';
+import IntegrationSection from '@/components/blocks/integration';
+import Integration2Section from '@/components/blocks/integration2';
+import PricingSection from '@/components/blocks/pricing';
+import FaqSection from '@/components/blocks/faqs';
+import TestimonialsSection from '@/components/blocks/testimonials';
+import NewsletterCard from '@/components/blocks/newsletter-card';
 
 export function HomePage() {
   return (
     <div className="flex flex-col">
       <HeroSection />
       <LogoCloudSection />
-      <Suspense>
-        <FeaturesSection />
-        <Features2Section />
-        <CallToActionSection />
-        <StatsSection />
-        <IntegrationSection />
-        <Integration2Section />
-        <PricingSection />
-        <FaqSection />
-        <TestimonialsSection />
-        <NewsletterCard />
-      </Suspense>
+      <FeaturesSection />
+      <Features2Section />
+      <CallToActionSection />
+      <StatsSection />
+      <IntegrationSection />
+      <Integration2Section />
+      <PricingSection />
+      <FaqSection />
+      <TestimonialsSection />
+      <NewsletterCard />
     </div>
   );
 }
